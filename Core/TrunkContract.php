369dd24384acff4f9d58c3a13ffd9cb71f124363
Nikola Plavsic <nikolaplavisc@gmail.com>
2015-10-23 16:08:21 +0200

    Changes to TrunkContract - Same functions for Model and Collection

--- Core/TrunkContract.php
+++ Core/TrunkContract.php
@@ -31,25 +31,6 @@ interface TrunkContract
 	 */
 	public function put($data);
 
-	/**
-	 * Add item to trunk
-	 * 
-	 * @param  mixed  $data
-	 * @param  boolean $included
-	 * 
-	 * @return void
-	 */
-	public function putItem($item);
-
-	/**
-	 * Add collection to trunk
-	 * 
-	 * @param  mixed  $data
-	 * @param  boolean $included
-	 * 
-	 * @return void
-	 */
-	public function putCollection($collection);
 
 	/**
 	 * Check cache for item or collection
@@ -62,26 +43,6 @@ interface TrunkContract
 	public function has($key, $type);
 
 	/**
-	 * Check if item exists in cache
-	 * 
-	 * @param  mixed  $id
-	 * @param  mixed  $type
-	 * 
-	 * @return boolean
-	 */
-	public function hasItem($id, $type);
-
-	/**
-	 * Check if collection exists in cache
-	 * 
-	 * @param  array  $params
-	 * @param  mixed  $type
-	 * 
-	 * @return boolean
-	 */
-	public function hasCollection($params, $type);
-
-	/**
 	 * Get item or collection from cache
 	 * 
 	 * @param  mixed $key
@@ -92,34 +53,14 @@ interface TrunkContract
 	public function get($key, $type);
 
 	/**
-	 * Get item by id and type
-	 * 
-	 * @param  mixed $id
-	 * @param  mixed $type
-	 * 
-	 * @return Model | null
-	 */
-	public function getItem($id, $type);
-
-	/**
-	 * Get collection by params and type
-	 * 
-	 * @param  array $params
-	 * @param  mixed $type
-	 * 
-	 * @return Model | null
-	 */
-	public function getCollection(array $params, $type);
-
-	/**
 	 * Clear item from cache
 	 * 
-	 * @param  mixed $id
+	 * @param  mixed $key
 	 * @param  mixed $type
 	 * 
 	 * @return void
 	 */
-	public function forget($id, $type);
+	public function forget($key, $type);
 
 	/**
 	 * Clear type from cache
