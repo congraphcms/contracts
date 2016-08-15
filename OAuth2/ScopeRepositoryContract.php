f06b547320d3b71dfb746c6cd241b998376f327b
Nikola Plavsic <nikolaplavisc@gmail.com>
2016-08-15 23:47:33 +0200

    adde ScopeRepository contract to OAuth2 package

---
+++ OAuth2/ScopeRepositoryContract.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * This file is part of the cookbook/contracts package.
+ *
+ * (c) Nikola Plavšić <nikolaplavsic@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cookbook\Contracts\OAuth2;
+
+use Cookbook\Contracts\Core\RepositoryContract;
+
+/**
+ * Interface for Scope Repository class
+ *
+ * @uses  Cookbook\Contracts\Core\RepositoryContract
+ * 
+ * @author  	Nikola Plavšić <nikolaplavsic@gmail.com>
+ * @copyright  	Nikola Plavšić <nikolaplavsic@gmail.com>
+ * @package 	cookbook/contracts
+ * @since 		0.1.0-alpha
+ * @version  	0.1.0-alpha
+ */
+interface ScopeRepositoryContract
+{
+	
+}
